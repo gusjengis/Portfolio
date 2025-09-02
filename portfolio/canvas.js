79b6780218f694668c73d4bac17ecd84d1283650
gusjengis <anthony.j.green@outlook.com>
2025-09-01 17:18:31 -0700

    added server, site files, and a flake so I can run node

---
+++ portfolio/canvas.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById('backgroundCanvas');
+var draw = canvas.getContext('2d');
+var backgroundImage =  document.createElement('canvas');
+var backDraw = backgroundImage.getContext('2d');
+
+function canvasOnLoad(){
+    generateBackgroundImage();
+    canvasResize();
+}
+
+function canvasResize(){
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+    applyBackgroundImage();
+}
+
+function generateBackgroundImage(){
+    
+    backgroundImage.width = 100;//window.screen.width;
+    backgroundImage.height = 100;//window.screen.height;
+    let height = backgroundImage.height;
+    let width = backgroundImage.width;
+    for(x = 0; x < width; x++){
+        for(y = 0; y < height; y++){
+
+            let r = 255*x/width;
+
+            let g = 255*(y)/height;// + (width - x);
+
+            let b = 150*(width-x)/width;
+
+            backDraw.fillStyle = 'rgb('+r+','+g+','+b+')';
+
+            backDraw.fillRect(x,y,1,1);
+        }
+    }
+}
+
+function applyBackgroundImage(){
+    draw.imageSmoothingEnabled = true;
+    draw.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+}
---
+++ portfolio/main.js
@@ -0,0 +1,166 @@
+var windowArr = [];
+var windowCounter = 0;
+var shortcutArr = [];
+var queryString = "";
+var currentPage;
+var pixelRatio = window.devicePixelRatio;
+
+window.onload = function() {
+    createShortcuts();
+    windowArr.push(new Window());
+    windowArr[windowArr.length - 1].home();
+    windowCounter++;
+    windowArr[windowArr.length - 1].setWPercent("96%");
+    windowArr[windowArr.length - 1].setHPercent("96%");
+    resize();
+    canvasOnLoad();
+    if (typeof loadPage == "function") {
+        loadPage();
+    }
+    buildTaskbar();
+    buildQS();
+    resize();
+}
+window.addEventListener('resize', resize);
+
+function resize() {
+    pixelRatio = window.devicePixelRatio;
+    canvasResize();
+    for (i = 0; i < windowArr.length; i++) {
+        windowArr[i].resizeWindow();
+        windowArr[i].resizeFuncs();
+    }
+}
+
+document.onclick = function() {
+
+    for (i = 0; i < windowArr.length; i++) {
+        if (windowArr[i].navBarClicked) {
+            windowArr[i].navBarClicked = false;
+            windowArr[i].fixPos();
+            if (windowArr[i].iframe != null)
+                windowArr[i].iframe.style.pointerEvents = "all";
+        }
+    }
+    buildQS();
+}
+
+let prevClientX;
+let prevClientY;
+document.onmousemove = function(e) {
+    let movementX = e.clientX - prevClientX;
+    let movementY = e.clientY - prevClientY;
+    prevClientX = e.clientX;
+    prevClientY = e.clientY;
+    for (i = 0; i < windowArr.length; i++) {
+        var windowPointer = windowArr[i];
+        if (windowPointer.clicked && windowPointer.window.style.cursor != "default" && windowPointer.window.style.cursor != "") {
+            switch (windowPointer.window.style.cursor) {
+                case "e-resize":
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width + movementX + "px";
+                    break;
+                case "w-resize":
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width - movementX + "px";
+                    windowPointer.window.style.left = windowPointer.window.getBoundingClientRect().left + movementX + "px";
+                    break;
+                case "s-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height + movementY + "px";
+                    break;
+                case "n-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height - movementY + "px";
+                    windowPointer.window.style.top = windowPointer.window.getBoundingClientRect().top + movementY + "px";
+                    break;
+                case "nw-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height - movementY + "px";
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width - movementX + "px";
+                    windowPointer.window.style.left = windowPointer.window.getBoundingClientRect().left + movementX + "px";
+                    windowPointer.window.style.top = windowPointer.window.getBoundingClientRect().top + movementY + "px";
+                    break;
+                case "sw-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height + movementY + "px";
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width - movementX + "px";
+                    windowPointer.window.style.left = windowPointer.window.getBoundingClientRect().left + movementX + "px";
+                    break;
+                case "ne-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height - movementY + "px";
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width + movementX + "px";
+                    windowPointer.window.style.top = windowPointer.window.getBoundingClientRect().top + movementY + "px";
+                    break;
+                case "se-resize":
+                    windowPointer.window.style.height = windowPointer.window.getBoundingClientRect().height + movementY + "px";
+                    windowPointer.window.style.width = windowPointer.window.getBoundingClientRect().width + movementX + "px";
+                    break;
+            }
+            windowPointer.fixPos2();
+            windowPointer.restore();
+            windowPointer.resizeFuncs();
+            windowPointer.resizeWindow();
+
+        } else if (windowPointer.navBarClicked && ((windowPointer.sizeMode == 0) || (windowPointer.sizeMode != 0 && (e.movementX > 1 || e.movementY > 1)))) {// e.movementX > 1 || e.movementY > 1)){
+            windowPointer.restore();
+            if (windowPointer.fullscreen) {
+                windowPointer.exitFullscreen();
+                windowPointer.x = 0.02 * document.documentElement.clientWidth;
+                windowPointer.y = 0;
+                windowPointer.fixPos();
+            }
+
+            // var zoom = window.devicePixelRatio;
+            windowPointer.window.style.left = windowPointer.window.getBoundingClientRect().left + movementX + "px";
+            windowPointer.window.style.top = windowPointer.window.getBoundingClientRect().top + movementY + "px";
+            windowPointer.x += movementX;
+            windowPointer.y += movementY;
+        }
+    }
+}
+
+document.onmousedown = function() {
+    for (i = 0; i < windowArr.length; i++) {
+        var windowPointer = windowArr[i];
+        if (windowPointer.iframe != null) { windowPointer.iframe.style.pointerEvents = "none"; }
+    }
+}
+
+document.onmouseup = function() {
+    for (i = 0; i < windowArr.length; i++) {
+        var windowPointer = windowArr[i];
+        if (windowPointer.iframe != null) { windowPointer.iframe.style.pointerEvents = "auto"; }
+        windowPointer.clicked = false;
+        if (windowPointer.sizeMode == 0) {
+            windowPointer.setWPercent(windowPointer.getRawWPercent());
+            windowPointer.setHPercent(windowPointer.getRawHPercent());
+        }
+    }
+}
+
+function clearAllWindows() {
+    for (i = 0; i < windowArr.length; i++) {
+        windowArr[i].close();
+    }
+    windowArr = [];
+}
+
+/// Query String
+
+function resetQS() {
+    queryString = "?";
+}
+
+function buildQS() {
+    resetQS();
+    for (i = 0; i < windowArr.length; i++) {
+        queryString += "w" + i + "=" + windowArr[i].getStateString() + "";
+        if (i < windowArr.length - 1) {
+            queryString += "+";
+        }
+    }
+    if (windowArr.length == 0) queryString = "";
+    window.history.pushState({}, "", currentPage + queryString);
+}
+
+// function decimalToBinary(input){
+//     if(Math.floor(input/2) == 0){
+//         return ""+input%2;
+//     }
+//     return decimalToBinary(Math.floor(input/2)) + "" + input%2;
+// }
