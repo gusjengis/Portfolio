e6d02819035596dab7283b0bd24ff18b37450928
gusjengis <anthony.j.green@outlook.com>
2025-09-01 21:45:40 -0700

    allowed formatter to style everything

--- portfolio/canvas.js
+++ portfolio/canvas.js
@@ -1,42 +1,42 @@
 var canvas = document.getElementById('backgroundCanvas');
 var draw = canvas.getContext('2d');
-var backgroundImage =  document.createElement('canvas');
+var backgroundImage = document.createElement('canvas');
 var backDraw = backgroundImage.getContext('2d');
 
-function canvasOnLoad(){
-    generateBackgroundImage();
-    canvasResize();
+function canvasOnLoad() {
+        generateBackgroundImage();
+        canvasResize();
 }
 
-function canvasResize(){
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
-    applyBackgroundImage();
+function canvasResize() {
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight;
+        applyBackgroundImage();
 }
 
-function generateBackgroundImage(){
-    
-    backgroundImage.width = 100;//window.screen.width;
-    backgroundImage.height = 100;//window.screen.height;
-    let height = backgroundImage.height;
-    let width = backgroundImage.width;
-    for(x = 0; x < width; x++){
-        for(y = 0; y < height; y++){
+function generateBackgroundImage() {
 
-            let r = 255*x/width;
+        backgroundImage.width = 100;//window.screen.width;
+        backgroundImage.height = 100;//window.screen.height;
+        let height = backgroundImage.height;
+        let width = backgroundImage.width;
+        for (x = 0; x < width; x++) {
+                for (y = 0; y < height; y++) {
 
-            let g = 255*(y)/height;// + (width - x);
+                        let r = 255 * x / width;
 
-            let b = 150*(width-x)/width;
+                        let g = 255 * (y) / height;// + (width - x);
 
-            backDraw.fillStyle = 'rgb('+r+','+g+','+b+')';
+                        let b = 150 * (width - x) / width;
 
-            backDraw.fillRect(x,y,1,1);
+                        backDraw.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+                        backDraw.fillRect(x, y, 1, 1);
+                }
         }
-    }
 }
 
-function applyBackgroundImage(){
-    draw.imageSmoothingEnabled = true;
-    draw.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+function applyBackgroundImage() {
+        draw.imageSmoothingEnabled = true;
+        draw.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 }
--- portfolio/desktopShortcut.js
+++ portfolio/desktopShortcut.js
@@ -1,147 +1,147 @@
 class desktopShortcut {
-    constructor(name, text, imgSrc, onclick){
-        this.name = name;
-        this.text = text;
-        this.imgSrc = imgSrc;
-        this.onclick = onclick;
-        this.body;
-        this.create();
+        constructor(name, text, imgSrc, onclick) {
+                this.name = name;
+                this.text = text;
+                this.imgSrc = imgSrc;
+                this.onclick = onclick;
+                this.body;
+                this.create();
 
-    }
+        }
 
-    create() {
-        this.body = createElement("div", this.name, "desktopShortcut", document.getElementById("newWindowMenu"));
-        let image = createElement("img", this.name+"Image", "shortcutIcon", this.body);
-        createTextElement("p", this.name+"Text", "shortcutText text", this.text, this.body);
+        create() {
+                this.body = createElement("div", this.name, "desktopShortcut", document.getElementById("newWindowMenu"));
+                let image = createElement("img", this.name + "Image", "shortcutIcon", this.body);
+                createTextElement("p", this.name + "Text", "shortcutText text", this.text, this.body);
 
-        let img = new Image(32,32);
-        img.src = this.imgSrc;
-        image.src = img.src;
+                let img = new Image(32, 32);
+                img.src = this.imgSrc;
+                image.src = img.src;
 
-        this.body.onclick = this.onclick;
-    }
+                this.body.onclick = this.onclick;
+        }
 }
 
 function createWindow() {
-    windowArr.push(new Window());
-    windowArr[windowArr.length-1].focus();
-    windowCounter++;
-    buildQS();
-    buildTaskbar();
-    for(i=0; i<windowArr.length-1; i++){
-        windowArr[i].guranteeClickability();
-    }
+        windowArr.push(new Window());
+        windowArr[windowArr.length - 1].focus();
+        windowCounter++;
+        buildQS();
+        buildTaskbar();
+        for (i = 0; i < windowArr.length - 1; i++) {
+                windowArr[i].guranteeClickability();
+        }
 
 }
 
 function createInitWindow(page, zIndex, xPercent, yPercent, wPercent, hPercent, sizeMode) {
-    windowArr.push(new Window());
-    windowArr[windowArr.length-1].focus();
-    windowCounter++;
-    eval("windowArr[windowArr.length-1]."+page);
-    windowArr[windowArr.length-1].setZIndex(zIndex);
-    windowArr[windowArr.length-1].setXPercent(yPercent);
-    windowArr[windowArr.length-1].setYPercent(xPercent);
-    windowArr[windowArr.length-1].setWPercent(wPercent);
-    windowArr[windowArr.length-1].setHPercent(hPercent);
-    windowArr[windowArr.length-1].sizeModeSwitch(Number(sizeMode));
-    windowArr[windowArr.length-1].unfixPos();
-    windowArr[windowArr.length-1].fixPos();
-    buildQS();
-    buildTaskbar();
+        windowArr.push(new Window());
+        windowArr[windowArr.length - 1].focus();
+        windowCounter++;
+        eval("windowArr[windowArr.length-1]." + page);
+        windowArr[windowArr.length - 1].setZIndex(zIndex);
+        windowArr[windowArr.length - 1].setXPercent(yPercent);
+        windowArr[windowArr.length - 1].setYPercent(xPercent);
+        windowArr[windowArr.length - 1].setWPercent(wPercent);
+        windowArr[windowArr.length - 1].setHPercent(hPercent);
+        windowArr[windowArr.length - 1].sizeModeSwitch(Number(sizeMode));
+        windowArr[windowArr.length - 1].unfixPos();
+        windowArr[windowArr.length - 1].fixPos();
+        buildQS();
+        buildTaskbar();
 }
 
-const newWindow = function(){
-    createWindow();
+const newWindow = function () {
+        createWindow();
 }
 
-const graph = function(){
-    createWindow();
-    windowArr[windowArr.length-1].graph();
+const graph = function () {
+        createWindow();
+        windowArr[windowArr.length - 1].graph();
 }
 
-const paint = function(){
-    createWindow();
-    windowArr[windowArr.length-1].paint();
+const paint = function () {
+        createWindow();
+        windowArr[windowArr.length - 1].paint();
 }
 function createShortcuts() {
-    // newWindowButton = new desktopShortcut("newWindow", "New Window", "assets/homeIcon.png", newWindow);
+        // newWindowButton = new desktopShortcut("newWindow", "New Window", "assets/homeIcon.png", newWindow);
 
-    // graphButton = new desktopShortcut("newGraph", "Graph", "assets/graphIcon.png", graph);
+        // graphButton = new desktopShortcut("newGraph", "Graph", "assets/graphIcon.png", graph);
 
-    // paintButton = new desktopShortcut("newGraph", "Paint", "assets/graphIcon.png", paint);
+        // paintButton = new desktopShortcut("newGraph", "Paint", "assets/graphIcon.png", paint);
 }
 
-function buildTaskbar(){
-    clearTaskbar();
-    let addButton = createElement("div", "newWindowButton", "newWindowButton", document.getElementById("taskbar"));
-    createTextElement("h1", "newWindowIcon", "taskbarIconText", "+", addButton);
-    addButton.onclick = function(){
-        if(windowArr.length<21){
-            createWindow();
+function buildTaskbar() {
+        clearTaskbar();
+        let addButton = createElement("div", "newWindowButton", "newWindowButton", document.getElementById("taskbar"));
+        createTextElement("h1", "newWindowIcon", "taskbarIconText", "+", addButton);
+        addButton.onclick = function () {
+                if (windowArr.length < 21) {
+                        createWindow();
+                }
         }
-    }
-    for(z=0; z<windowArr.length; z++){
-        createTaskbarIcon(windowArr[z], z);
-    }
-    let clearButton = createElement("div", "clearButton", "clearButton", document.getElementById("taskbar"));
-    clearButton.onclick = function(){
-        for(m=0; m<windowArr.length; m++){
-            windowArr[m].minimize();
+        for (z = 0; z < windowArr.length; z++) {
+                createTaskbarIcon(windowArr[z], z);
         }
-    }
+        let clearButton = createElement("div", "clearButton", "clearButton", document.getElementById("taskbar"));
+        clearButton.onclick = function () {
+                for (m = 0; m < windowArr.length; m++) {
+                        windowArr[m].minimize();
+                }
+        }
 }
 
-function createTaskbarIcon(window, index){
-    
-    let icon = createElement("div", "taskbarIcon"+window.ID, "taskbarIcon", document.getElementById("taskbar"));
-    icon.window = window;
-    let label = createTextElement("h1", "", "text taskbarIconText", index+1, icon);
-    if(window.display == "hidden"){
-        icon.classList.add("iconIsMinimized");
-        label.classList.add("iconIsMinimized");
-    } else if(window.isFocused()){
-        icon.classList.add("iconIsFocused");
-    }
-    
-    icon.onclick = function(){
-        if(this.window.isFocused()){
-            this.window.minimize();
-            this.window.window.style.visibility = "hidden";
-        } else {
-            this.window.focus();
+function createTaskbarIcon(window, index) {
+
+        let icon = createElement("div", "taskbarIcon" + window.ID, "taskbarIcon", document.getElementById("taskbar"));
+        icon.window = window;
+        let label = createTextElement("h1", "", "text taskbarIconText", index + 1, icon);
+        if (window.display == "hidden") {
+                icon.classList.add("iconIsMinimized");
+                label.classList.add("iconIsMinimized");
+        } else if (window.isFocused()) {
+                icon.classList.add("iconIsFocused");
         }
-    }   
-    icon.addEventListener("mouseenter",function(e){
-        for(i=0; i<windowArr.length; i++){
-            windowArr[i].window.style.backgroundColor = "rgba(0,0,0,0)";
-            windowArr[i].navBar.style.visibility = "hidden";
-            windowArr[i].content.style.visibility = "hidden";
-            if(windowArr[i].sizeMode < 10){
-                windowArr[i].title.style.visibility = "visible";
-            }
+
+        icon.onclick = function () {
+                if (this.window.isFocused()) {
+                        this.window.minimize();
+                        this.window.window.style.visibility = "hidden";
+                } else {
+                        this.window.focus();
+                }
         }
-        this.window.window.style.visibility = "visible";
-        this.window.window.style.backgroundColor = "rgba(0,0,0,0.4)";
-        this.window.navBar.style.visibility = "inherit";
-        this.window.content.style.visibility = "inherit";
-        this.window.window.style.zIndex=windowArr.length+1;
-    });
-    icon.addEventListener("mouseleave", function(){
-        for(i=0; i<windowArr.length; i++){
-            windowArr[i].window.style.backgroundColor = "rgba(0,0,0,0.4)";
-            windowArr[i].navBar.style.visibility = "inherit";
-            windowArr[i].content.style.visibility = "inherit";
-            windowArr[i].window.style.visibility = windowArr[i].display;
-            windowArr[i].setZIndex(windowArr[i].zIndex);
-            if(windowArr[i].display == "none"){
-                windowArr[i].minimize();
-            }
-            windowArr[i].minSizeMaintenance();
-        }
-    });
+        icon.addEventListener("mouseenter", function (e) {
+                for (i = 0; i < windowArr.length; i++) {
+                        windowArr[i].window.style.backgroundColor = "rgba(0,0,0,0)";
+                        windowArr[i].navBar.style.visibility = "hidden";
+                        windowArr[i].content.style.visibility = "hidden";
+                        if (windowArr[i].sizeMode < 10) {
+                                windowArr[i].title.style.visibility = "visible";
+                        }
+                }
+                this.window.window.style.visibility = "visible";
+                this.window.window.style.backgroundColor = "rgba(0,0,0,0.4)";
+                this.window.navBar.style.visibility = "inherit";
+                this.window.content.style.visibility = "inherit";
+                this.window.window.style.zIndex = windowArr.length + 1;
+        });
+        icon.addEventListener("mouseleave", function () {
+                for (i = 0; i < windowArr.length; i++) {
+                        windowArr[i].window.style.backgroundColor = "rgba(0,0,0,0.4)";
+                        windowArr[i].navBar.style.visibility = "inherit";
+                        windowArr[i].content.style.visibility = "inherit";
+                        windowArr[i].window.style.visibility = windowArr[i].display;
+                        windowArr[i].setZIndex(windowArr[i].zIndex);
+                        if (windowArr[i].display == "none") {
+                                windowArr[i].minimize();
+                        }
+                        windowArr[i].minSizeMaintenance();
+                }
+        });
 }
 
-function clearTaskbar(){
-    document.getElementById("taskbar").innerHTML = "";
+function clearTaskbar() {
+        document.getElementById("taskbar").innerHTML = "";
 }
